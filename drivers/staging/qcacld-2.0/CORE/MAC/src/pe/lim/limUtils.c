68e3570953f2fbff2b6b22e396d3298748b52ad9
Nathan Chancellor <natechancellor@gmail.com>
2018-05-08 16:48:05 -0700

    qqcacld-2.0: squashed GCC7 fixes.
    qcacld-2.0: Fix misleading indentation warnings in i_trim
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in hdd_alloc_station_adapter
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in hdd_state_info_dump
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in hdd_update_config_dat
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limStaSendAddBssPreAssoc
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in __limIbssSearchAndDeletePeer
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limUpdateAssocStaDatas
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in __limHandleBeacon
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limProcessMlmRemoveKeyReq
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limHandleDelBssInReAssocContext
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warnings in limRemovePBCSessions
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limExtractApCapability
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limSendAddBARsp
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limSendDelBAInd
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limEnableHT20Protection
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limEnableHTNonGfProtection
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limEnableHTNonGfProtection
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in limEnableHtRifsProtection
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix unreachable switch statement warning in sapComputeSpectWeight
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in ConvertQosMapsetFrame
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix misleading indentation warning in dfs_process_phyerr_owl
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    qcacld-2.0: Fix bool compare warning in dbglog_report_enable
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    
    ---
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limUtils.c
+++ drivers/staging/qcacld-2.0/CORE/MAC/src/pe/lim/limUtils.c
@@ -4097,8 +4097,8 @@ limEnableHT20Protection(tpAniSirGlobal pMac, tANI_U8 enable,
     if(!psessionEntry->htCapability)
         return eSIR_SUCCESS; // this protection  is only for HT stations.
 
-        //overlapping protection configuration check.
-        if(overlap) {
+    //overlapping protection configuration check.
+    if(overlap) {
         } else {
             //normal protection config check
             if (LIM_IS_AP_ROLE(psessionEntry) &&
@@ -4301,8 +4301,8 @@ limEnableHTNonGfProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     if(!psessionEntry->htCapability)
         return eSIR_SUCCESS; // this protection  is only for HT stations.
 
-        //overlapping protection configuration check.
-        if(overlap) {
+    //overlapping protection configuration check.
+    if(overlap) {
         } else {
             //normal protection config check
             if (LIM_IS_AP_ROLE(psessionEntry) &&
@@ -4366,8 +4366,8 @@ limEnableHTLsigTxopProtection(tpAniSirGlobal pMac, tANI_U8 enable,
     if(!psessionEntry->htCapability)
         return eSIR_SUCCESS; // this protection  is only for HT stations.
 
-        //overlapping protection configuration check.
-        if(overlap) {
+    //overlapping protection configuration check.
+    if(overlap) {
         } else {
             //normal protection config check
             if (LIM_IS_AP_ROLE(psessionEntry) &&
@@ -4432,8 +4432,8 @@ limEnableHtRifsProtection(tpAniSirGlobal pMac, tANI_U8 enable,
         return eSIR_SUCCESS; // this protection  is only for HT stations.
 
 
-        //overlapping protection configuration check.
-        if(overlap) {
+    //overlapping protection configuration check.
+    if(overlap) {
         } else {
              //normal protection config check
             if (LIM_IS_AP_ROLE(psessionEntry) &&
